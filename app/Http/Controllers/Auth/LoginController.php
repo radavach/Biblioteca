877d93d41c1c9483e950df29782fc3d2bddd30a4
Christian Daniel Ramos Vallejo <christian.rvallejo@alumnos.udg.mx>
2019-03-07 02:02:09 -0600

    modificando antes de modelo

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,13 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
+
+    protected function redirectTo()
+    {
+        session(['apodo' => 'mich']);
+        return '/empleados';
+    }
 
     /**
      * Create a new controller instance.
