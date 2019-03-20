accc2c813ea194baaf8527ec0891e7e4b46d27c3
Christian Daniel Ramos Vallejo <christian.rvallejo@alumnos.udg.mx>
2019-03-20 16:37:39 -0600

    modificando el controlador

--- app/Http/Controllers/EmpleadosController.php
+++ app/Http/Controllers/EmpleadosController.php
@@ -41,6 +41,10 @@ class EmpleadosController extends Controller
     public function update()
     {
         // poner rutas
+        $dependencia->dependencia = $request->input('dependencia');
+        $dependencia->clave = $request->clave;
+        $dependencia->save();
+        return redirect()->route('dependencias.show', $dependencia->id);
     }
     public function delete()
     {
@@ -49,5 +53,10 @@ class EmpleadosController extends Controller
     public function store()
     {
 
+    }
+    public function destroy(Empleado $empleado)
+    {
+        $empleado->delete();
+        return redirect()->route('empleados.index');
     }
 }
